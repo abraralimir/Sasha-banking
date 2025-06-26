ae594fa7f235573d6079fea50152c75c19214f8d
Mir Abrar Ali <alimirabrar@gmail.com>
2025-06-26 08:27:04 +0000

    let me input a video that can run in backgorund on loop guide me where s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="animated-background"></div>
-          {children}
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="background-video"
+            key="background-video"
+          >
+            <source src="https://cdn.pixabay.com/video/2024/05/27/211598-945761358_large.mp4" type="video/mp4" />
+          </video>
+          <main className="relative z-10">{children}</main>
           <Toaster />
         </ThemeProvider>
       </body>
